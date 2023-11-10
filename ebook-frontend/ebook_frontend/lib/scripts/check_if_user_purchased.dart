76a1306085dfecaad11b0798bf3218a9a2785dd2
Shady ayman <sf2100653@tkh.edu.eg>
2023-11-10 04:28:31 +0200

    Update check_if_user_purchased.dart

--- ebook-frontend/ebook_frontend/lib/scripts/check_if_user_purchased.dart
+++ ebook-frontend/ebook_frontend/lib/scripts/check_if_user_purchased.dart
@@ -1,21 +1,16 @@
 import 'package:ebook_frontend/scripts/api_calls.dart';
 
 Future<bool> hasUserPurchasedBook(book_id) async {
-  var userLibary = await userLibrary();
-  List book_ids = List.from(userLibary.map((book) {
-    print(book_id);
-    return {
-      book['id']
-    };
-  }));
-  print('These are the book ids${book_ids}');
-  if (book_ids.any((book_id)=>book_id == book_id)) {
-    print('The user has the book with id${book_id}');
-
+  var userlib = await userLibrary();
+  // Ensure the list contains only the book ids and not sets
+  List book_ids = userlib.map((book) => book['id']).toList();
+  print('These are the book ids $book_ids');
+  
+  if (book_ids.contains(book_id)) {
+    print('The user has the book with id $book_id');
     return true;
   } else {
-    print('The user does not have the book with id${book_id}');
-
+    print('The user does not have the book with id $book_id');
     return false;
   }
 }
